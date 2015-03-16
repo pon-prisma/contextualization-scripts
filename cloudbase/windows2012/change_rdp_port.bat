3fb557c1092eef83ede80b8b067d7cadcb48a19d
Marica Antonacci <marica.antonacci@gmail.com>
2015-03-16 21:50:51 +0100

    Update change_rdp_port.bat
    Added registry update to enable RDP

--- cloudbase/windows2012/change_rdp_port.bat
+++ cloudbase/windows2012/change_rdp_port.bat
@@ -4,6 +4,8 @@ ECHO > "%TMP%\myregfile.reg" Windows Registry Editor Version 5.00
 
 ECHO >> "%TMP%\myregfile.reg" [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp]
 ECHO >> "%TMP%\myregfile.reg" "PortNumber"=dword:0000344d
+ECHO >> "%TMP%\myregfile.reg" [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server]
+ECHO >> "%TMP%\myregfile.reg" "fDenyTSConnections"=dword:0 
 
 regedit.exe /s %TMP%\myregfile.reg
 
